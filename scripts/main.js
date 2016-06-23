c347e410b4b1f2d2dc4ac1ff09bb0490c119ecb3
Medrano, Tony (NYC-MRM) <Tony.Medrano@mrm-mccann.com>
2016-06-23 11:17:34 -0400

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+
+
+var game = new Phaser.Game(600, 400, Phaser.AUTO);
+game.state.add('state1', demo.state1);
+game.state.start('state1');
---
+++ scripts/state.js
@@ -0,0 +1,17 @@
+
+var demo = {};
+demo.state1 = function () {};
+demo.state1.prototype = {
+
+	preload: function () {
+		// body...
+	},
+
+	create: function () {
+		// body...
+	},
+
+	update: function () {
+		// body...
+	}
+}
